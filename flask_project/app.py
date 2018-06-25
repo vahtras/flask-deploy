63b287739eca0eba3b206db9557daa99fd35849a
Olav Vahtras <vahtras@kth.se>
2018-06-25 23:43:05 +0200

    juuli

--- flask_project/app.py
+++ flask_project/app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/data')
 def names():
     data = {
-        "first_names": ["Jon", "Jacob", "Julie", "Jenny", "Joan", "Jim"]
+        "first_names": ["Jon", "Jacob", "Juulie", "Jenny", "Joan", "Jim"]
     }
     return jsonify(data)
 
