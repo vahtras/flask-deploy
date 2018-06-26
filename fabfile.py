52e2fa0e5a071a2547fa4f5842b9bbdba9c5f3d9
Olav Vahtras <vahtras@kth.se>
2018-06-26 16:39:42 +0200

    updates

--- fabfile.py
+++ fabfile.py
@@ -11,14 +11,14 @@ from fabric.contrib.files import exists
 ##############
 
 project = 'flask_project'
-local_app_dir = './{}'.format(project)
+_local_app_dir = lambda app: './%s' % app
 local_config_dir = './config'
 
-remote_app_dir = '/home/www'
-remote_git_dir = '/home/git'
-remote_flask_dir = '{}/{}'.format(remote_app_dir, project)
-remote_nginx_dir = '/etc/nginx/sites-available'
-remote_supervisor_dir = '/etc/supervisor/conf.d'
+REMOTE_WWW_DIR = '/home/www'
+REMOTE_GIT_DIR = '/home/git'
+_remote_flask_dir = lambda app: '{}/{}'.format(REMOTE_WWW_DIR, app)
+REMOTE_NGINX_DIR = '/etc/nginx/sites-available'
+REMOTE_SUPERVISOR_DIR = '/etc/supervisor/conf.d'
 
 env.hosts = ['104.200.30.58']  # replace with IP address or hostname
 env.user = 'olav'
@@ -41,27 +41,27 @@ def install_requirements():
     sudo('apt-get install -y git')
 
 
-def install_flask():
+def install_flask(app):
     """
     1. Create project directories
     2. Create and activate a virtualenv
     3. Copy Flask files to remote host
     """
-    if exists(remote_app_dir) is False:
-        sudo('mkdir ' + remote_app_dir)
-        sudo('chown {u}:{u} {d}'.format(u=env.user, d=remote_app_dir))
-    if exists(remote_flask_dir) is False:
-        run('mkdir -p {}/{}'.format(remote_flask_dir, project))
-    with lcd(local_app_dir):
-        with cd(remote_flask_dir):
+    if exists(REMOTE_WWW_DIR) is False:
+        sudo('mkdir ' + REMOTE_WWW_DIR)
+        sudo('chown {u}:{u} {d}'.format(u=env.user, d=REMOTE_WWW_DIR))
+    if exists(_remote_flask_dir(app)) is False:
+        run('mkdir -p {}/{}'.format(_remote_flask_dir(app), app))
+    with lcd(_local_app_dir(app)):
+        with cd(_remote_flask_dir(app)):
             run('virtualenv venv3 -p python3')
             run('source venv3/bin/activate')
             run('pip install Flask==0.10.1')
-        with cd(remote_flask_dir):
-            put('*', './{}'.format(project), use_sudo=False)
+        with cd(_remote_flask_dir(app)):
+            put('*', './{}'.format(app), use_sudo=False)
 
 
-def configure_nginx():
+def configure_nginx(app):
     """
     1. Remove default nginx config file
     2. Create new config file
@@ -72,84 +72,88 @@ def configure_nginx():
     sudo('/etc/init.d/nginx start')
     if exists('/etc/nginx/sites-enabled/default'):
         sudo('rm /etc/nginx/sites-enabled/default')
-    if exists('/etc/nginx/sites-enabled/%s' % project) is False:
-        sudo('touch /etc/nginx/sites-available/%s' % project)
+    if exists('/etc/nginx/sites-enabled/%s' % app) is False:
+        sudo('touch /etc/nginx/sites-available/%s' % app)
         sudo(('ln -s /etc/nginx/sites-available/{p}' +
-             ' /etc/nginx/sites-enabled/{p}').format(p=project))
+             ' /etc/nginx/sites-enabled/{p}').format(p=app))
     with lcd(local_config_dir):
-        with cd(remote_nginx_dir):
-            put('./%s' % project, './', use_sudo=True)
+        with cd(REMOTE_NGINX_DIR):
+            put('./%s' % app, './', use_sudo=True)
     sudo('/etc/init.d/nginx restart')
 
 
-def configure_supervisor():
+def configure_supervisor(app):
     """
     1. Create new supervisor config file
     2. Copy local config to remote config
     3. Register new command
     """
-    if exists('/etc/supervisor/conf.d/%s.conf' % project) is False:
+    if exists('/etc/supervisor/conf.d/%s.conf' % app) is False:
         with lcd(local_config_dir):
-            with cd(remote_supervisor_dir):
-                put('./%s.conf' % project, './', use_sudo=True)
+            with cd(REMOTE_SUPERVISOR_DIR):
+                put('./%s.conf' % app, './', use_sudo=True)
                 sudo('supervisorctl reread')
                 sudo('supervisorctl update')
 
 
-def configure_git():
+def configure_git(app):
     """
     1. Setup bare Git repo
     2. Create post-receive hook
     """
-    if exists(remote_git_dir) is False:
-        sudo('mkdir ' + remote_git_dir)
-        sudo('chown {u}:{u} {d}'.format(u=env.user, d=remote_git_dir))
-        with cd(remote_git_dir):
-            run('mkdir %s.git' % project)
-            with cd('%s.git' % project):
+    if exists(REMOTE_GIT_DIR) is False:
+        sudo('mkdir ' + REMOTE_GIT_DIR)
+        sudo('chown {u}:{u} {d}'.format(u=env.user, d=REMOTE_GIT_DIR))
+        with cd(REMOTE_GIT_DIR):
+            run('mkdir %s.git' % app)
+            with cd('%s.git' % app):
                 run('git init --bare')
                 with lcd(local_config_dir):
                     with cd('hooks'):
+                        with open(local_config_dir + '/post-receive', 'w') as hook:
+                            hook.write("""#!/bin/sh
+GIT_WORK_TREE=/home/www/%s git checkout -f
+""" % app)
                         put('./post-receive', './', use_sudo=False)
                         sudo('chmod +x post-receive')
 
 
-def run_app():
+def run_app(app):
     """ Run the app! """
-    with cd(remote_flask_dir):
-        sudo('supervisorctl start %s' % project)
+    with cd(_remote_flask_dir(app)):
+        sudo('supervisorctl start %s' % app)
 
-def stop_app():
+def stop_app(app):
     """ Run the app! """
-    with cd(remote_flask_dir):
-        sudo('supervisorctl stop %s' % project)
+    with cd(_remote_flask_dir(app)):
+        sudo('supervisorctl stop %s' % app)
 
 
-def deploy():
+def deploy(app, repo='production'):
     """
     1. Copy new Flask files
     2. Restart gunicorn via supervisor
     """
-    with lcd(local_app_dir):
+    with lcd(_local_app_dir(app)):
         local('git add -A')
         commit_message = prompt("Commit message?")
         local('git commit -am "{0}"'.format(commit_message))
-        local('git push production master')
-        sudo('supervisorctl restart %s' % project)
+        local('git push %s master' % repo)
+        sudo('supervisorctl restart %s' % app)
 
-def restart():
-        sudo('supervisorctl restart %s' % project)
+def restart(app):
+        sudo('supervisorctl restart %s' % app)
 
 
-def rollback():
+def rollback(app, repo='production'):
     """
     1. Quick rollback in case of error
     2. Restart gunicorn via supervisor
     """
-    with lcd(local_app_dir):
+    with lcd(_local_app_dir(app)):
         local('git revert master  --no-edit')
-        local('git push production master')
-        sudo('supervisorctl restart %s' % project)
+        local('git push %s master' % repo)
+        sudo('supervisorctl restart %s' % app)
 
 
 def status():
@@ -157,17 +161,17 @@ def status():
     sudo('supervisorctl status')
 
 
-def create():
+def create(app):
     install_requirements()
-    install_flask()
-    configure_nginx()
-    configure_supervisor()
-    configure_git()
+    install_flask(app)
+    configure_nginx(app)
+    configure_supervisor(app)
+    configure_git(app)
 
-def clean():
-    stop_app()
-    sudo('rm -rf %s' % remote_app_dir)
-    sudo('rm -rf %s' % remote_git_dir)
-    sudo('rm -f /etc/supervisor/conf.d/%s.conf' % project)
-    sudo('rm -f /etc/nginx/sites-available/%s % project')
-    sudo('rm -f /etc/nginx/sites-enabled/%s % project')
+def clean(app):
+    stop_app(app)
+    sudo('rm -rf %s' % REMOTE_WWW_DIR)
+    sudo('rm -rf %s' % REMOTE_GIT_DIR)
+    sudo('rm -f /etc/supervisor/conf.d/%s.conf' % app)
+    sudo('rm -f /etc/nginx/sites-available/%s' % app)
+    sudo('rm -f /etc/nginx/sites-enabled/%s' % app)
