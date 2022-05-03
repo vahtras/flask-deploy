8f52ce17d22b6a3ebb7164b64466be7818813ffe
Olav Vahtras <vahtras@kth.se>
2022-05-03 16:36:27 +0200

    updatetests

--- test_fab.py
+++ test_fab.py
@@ -1,18 +1,14 @@
 import unittest
-import pytest
-import sys
-try:
-    from unittest.mock import patch, call, mock_open, MagicMock
-except ImportError:
-    from mock import patch, call, mock_open, MagicMock
+from unittest.mock import patch, call, MagicMock
+import textwrap
 
+import fabfile
 
-from fabfile import *
 
 @patch('fabfile.REMOTE_ROOT', '/www')
 @patch('fabfile.SERVER_IP', '123.456.789.00')
 @patch('fabfile.user', 'whom')
-@patch('invoke.tasks.isinstance') # necessary for mocking
+@patch('invoke.tasks.isinstance')  # necessary for mocking
 @patch('fabfile.exists')
 class TestFab(unittest.TestCase):
 
@@ -20,7 +16,7 @@ class TestFab(unittest.TestCase):
         self.c = MagicMock()
 
     def test_hello(self, *args):
-        hello(self.c)
+        fabfile.hello(self.c)
         self.c.run.assert_called_once_with('echo "Hello world!"')
 
 ###########
@@ -28,20 +24,20 @@ class TestFab(unittest.TestCase):
 ###########
 
     def test_site_dir(self, *args):
-        install_site_dir(self.c, 'foo.bar')
-        self.c.run.assert_called_once_with(f'mkdir -p /www/sites/foo.bar')
+        fabfile.install_site_dir(self.c, 'foo.bar')
+        self.c.run.assert_called_once_with('mkdir -p /www/sites/foo.bar')
 
     def test_install_venv(self, *args):
-        install_venv(self.c, 'foo.bar')
-        self.c.run.assert_called_once_with(f"""\
-virtualenv /www/sites/foo.bar/venv3 -p python3
-source /www/sites/foo.bar/venv3/bin/activate
-pip install -r /www/sites/foo.bar/requirements.txt
-"""
-        )
-        
+        fabfile.install_venv(self.c, 'foo.bar')
+        self.c.run.assert_called_once_with(textwrap.dedent(
+            """\
+            virtualenv /www/sites/foo.bar/venv3 -p python3
+            source /www/sites/foo.bar/venv3/bin/activate
+            pip install -r /www/sites/foo.bar/requirements.txt
+            """
+        ))
 #######
-# git #    
+# git #
 #######
 
     def test_configure_git_exists(self, *args):
@@ -51,7 +47,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
         with patch('fabfile.exists') as mock_exists:
             with patch('fabfile.print') as mock_print:
                 mock_exists.return_value = True
-                configure_git(self.c, 'foo.bar')
+                fabfile.configure_git(self.c, 'foo.bar')
 
         mock_print.assert_called_once_with(
             '/www/sites/foo.bar/git already exists'
@@ -59,28 +55,27 @@ pip install -r /www/sites/foo.bar/requirements.txt
         self.c.sudo.assert_not_called()
         self.c.cd.assert_not_called()
         self.c.run.assert_not_called()
-        
+
     def test_configure_git_new(self, *args):
         exists, _ = args
         exists.side_effect = [False, False]
 
-        configure_git(self.c, 'foo.bar')
+        fabfile.configure_git(self.c, 'foo.bar')
 
         post_receive_file = '/www/sites/foo.bar/git/hooks/post-receive'
-        post_receive_cmd = "#!/bin/sh\nGIT_WORK_TREE=/www/sites/foo.bar/src git checkout -f"
+        post_receive_cmd = "#!/bin/sh\nGIT_WORK_TREE=/www/sites/foo.bar/src git checkout --recurse-submodules -f"
         self.c.run.assert_has_calls([
             call('git init --bare /www/sites/foo.bar/git'),
             call('echo "%s" > %s' % (post_receive_cmd, post_receive_file)),
             call(f'chmod +x {post_receive_file}')
         ])
 
-
     def test_remote_git_dir(self, *args):
-        assert remote_git_dir('foo.bar')  == '/www/sites/foo.bar/git'
+        assert fabfile.remote_git_dir('foo.bar') == '/www/sites/foo.bar/git'
 
     def test_add_remote(self, *args):
         with patch('fabfile.subprocess.run') as fsr:
-            add_remote(self.c, 'foo.bar')
+            fabfile.add_remote(self.c, 'foo.bar')
         fsr.assert_called_once_with(
             'git remote add foo.bar whom@foo.bar:/www/sites/foo.bar/git',
             shell=True, check=True
@@ -88,14 +83,14 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
     def test_add_existing_remote(self, *args):
         with patch('fabfile.subprocess.run') as fsr:
-            fsr.side_effect = subprocess.CalledProcessError(1, 'yo')
+            fsr.side_effect = fabfile.subprocess.CalledProcessError(1, 'yo')
             with patch('fabfile.print') as fp:
-                add_remote(self.c, 'foo.bar')
+                fabfile.add_remote(self.c, 'foo.bar')
         fp.assert_called_once_with('Remote repository foo.bar exists')
 
     def test_add_remote_other(self, *args):
         with patch('fabfile.subprocess.run') as fsr:
-            add_remote(self.c, 'foo.bar', 'test.site')
+            fabfile.add_remote(self.c, 'foo.bar', 'test.site')
         fsr.assert_called_once_with(
             'git remote add foo.bar whom@test.site:/www/sites/foo.bar/git',
             shell=True, check=True
@@ -103,7 +98,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
     def test_push_remote(self, *args):
         with patch('fabfile.subprocess.run') as fsr:
-            push_remote(self.c, 'foo.bar')
+            fabfile.push_remote(self.c, 'foo.bar')
         fsr.assert_called_once_with(
             'git push foo.bar master:master',
             shell=True
@@ -113,14 +108,13 @@ pip install -r /www/sites/foo.bar/requirements.txt
 # flask #
 #########
 
-
     def test_install_flask_and_exists(self, *args):
         exists, _ = args
         exists.return_value = True
 
         with patch('fabfile.install_root'):
             with patch('fabfile.print') as p:
-                install_flask(self.c, 'foo.bar')
+                fabfile.install_flask(self.c, 'foo.bar')
         self.c.run.assert_not_called()
         p.assert_called_once_with('/www/sites/foo.bar/src exists')
 
@@ -130,7 +124,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
         with patch('fabfile.install_root'):
             with patch('fabfile.install_venv'):
-                install_flask(self.c, 'foo.bar')
+                fabfile.install_flask(self.c, 'foo.bar')
         self.c.run.assert_has_calls([
             call("mkdir -p /www/sites/foo.bar/src"),
             call(
@@ -140,20 +134,19 @@ pip install -r /www/sites/foo.bar/requirements.txt
         ])
 
     def test_remote_flask_dir(self, *args):
-        assert remote_flask_dir('foo.bar') == '/www/sites/foo.bar/src'
+        assert fabfile.remote_flask_dir('foo.bar') == '/www/sites/foo.bar/src'
 
     def test_install_root(self, *args):
         exists, _ = args
         exists.return_value = False
-        install_root(self.c)
+        fabfile.install_root(self.c)
         self.c.sudo.assert_has_calls([
-            call(f'mkdir -p /www'),
-            call(f'chown whom:whom /www'),
+            call('mkdir -p /www'),
+            call('chown whom:whom /www'),
         ])
 
     def test_remote_site(self, *args):
-        assert remote_site_dir('foo.bar') == '/www/sites/foo.bar'
-        
+        assert fabfile.remote_site_dir('foo.bar') == '/www/sites/foo.bar'
 
 #########
 # nginx #
@@ -161,10 +154,10 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
     def test_configure_nginx(self, *args):
         exists, _ = args
-        exists.side_effect=[True, True]
+        exists.side_effect = [True, True]
 
         with patch('fabfile.disable_nginx_default'):
-            configure_nginx(self.c, 'foo.bar')
+            fabfile.configure_nginx(self.c, 'foo.bar')
 
         self.c.sudo.assert_has_calls([
             call('/etc/init.d/nginx start'),
@@ -173,26 +166,25 @@ pip install -r /www/sites/foo.bar/requirements.txt
         ])
 
         self.c.put.assert_called_with(
-            f'./sites/foo.bar/etc/nginx/sites-available/foo.bar',
+            './sites/foo.bar/etc/nginx/sites-available/foo.bar',
             '/tmp/foo.bar'
         )
-
 
     def test_disable_default(self, *args):
         exists, _ = args
 
         exists.return_value = False
-        disable_nginx_default(self.c)
+        fabfile.disable_nginx_default(self.c)
         self.c.sudo.assert_not_called()
 
         exists.return_value = True
-        disable_nginx_default(self.c)
+        fabfile.disable_nginx_default(self.c)
         self.c.sudo.assert_called_once_with(
             'rm /etc/nginx/sites-enabled/default'
         )
 
     def test_setup_link(self, *args):
-        enable_link(self.c, 'foo.bar')
+        fabfile.enable_link(self.c, 'foo.bar')
         self.c.sudo.assert_has_calls([
             call('touch /etc/nginx/sites-available/foo.bar'),
             call(
@@ -200,11 +192,9 @@ pip install -r /www/sites/foo.bar/requirements.txt
                 ' /etc/nginx/sites-enabled/foo.bar'
             ),
         ])
-        
-        
 
     def test_scp(self, *args):
-        scp(self.c, 'foo', '/bar/baz')
+        fabfile.scp(self.c, 'foo', '/bar/baz')
         self.c.put.assert_called_once_with('foo', '/tmp/baz')
         self.c.sudo.assert_called_once_with('mv /tmp/baz /bar/baz')
 
@@ -216,7 +206,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
         exists, _ = args
         exists.return_value = True
 
-        configure_supervisor(self.c, 'foo.bar')
+        fabfile.configure_supervisor(self.c, 'foo.bar')
 
         self.c.sudo.assert_not_called()
         self.c.cd.assert_not_called()
@@ -224,7 +214,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
         exists.return_value = False
 
-        configure_supervisor(self.c, 'foo.bar')
+        fabfile.configure_supervisor(self.c, 'foo.bar')
 
         self.c.put.assert_called_with(
             './sites/foo.bar/etc/supervisor/conf.d/foo.bar.conf',
@@ -236,21 +226,16 @@ pip install -r /www/sites/foo.bar/requirements.txt
             call('supervisorctl update'),
         ])
 
-
-
-
     def test_run(self, *args):
-
-        start_app(self.c, 'foo.bar')
+        fabfile.start_app(self.c, 'foo.bar')
         self.c.sudo.assert_called_once_with('supervisorctl start foo.bar')
 
     def test_stop(self, *args):
-
-        stop_app(self.c, 'foo.bar')
+        fabfile.stop_app(self.c, 'foo.bar')
         self.c.sudo.assert_called_once_with('supervisorctl stop foo.bar')
 
     def test_status(self, *args):
-        status(self.c)
+        fabfile.status(self.c)
         self.c.sudo.assert_called_once_with('supervisorctl status')
 
 ##########
@@ -260,13 +245,13 @@ pip install -r /www/sites/foo.bar/requirements.txt
     def test_deploy(self, *args):
         with patch('fabfile.start_app') as mrun:
             with patch('fabfile.stop_app') as mstop:
-                restart_app(self.c, 'foo.bar')
+                fabfile.restart_app(self.c, 'foo.bar')
         mrun.assert_called_once_with(self.c, 'foo.bar')
         mstop.assert_called_once_with(self.c, 'foo.bar')
 
     def test_rollback(self, *args):
 
-        rollback(self.c, 'foo.bar')
+        fabfile.rollback(self.c, 'foo.bar')
 
         self.c.local.assert_has_calls([
             call('git revert master --no-edit'),
@@ -274,22 +259,19 @@ pip install -r /www/sites/foo.bar/requirements.txt
         ])
         self.c.sudo.assert_called_once_with('supervisorctl restart foo.bar')
 
-
-
-
 ###########
 # certbot #
 ###########
 
     def test_install_certbot(self, *args):
-        install_certbot(self.c)
+        fabfile.install_certbot(self.c)
         self.c.sudo.assert_has_calls([
             call('apt-get update'),
             call('apt-get install python-certbot-nginx'),
         ])
 
     def test_install_cert(self, *args):
-        install_cert(self.c)
+        fabfile.install_cert(self.c)
         self.c.sudo.assert_called_once_with(
             'certbot --nginx'
         )
@@ -300,7 +282,7 @@ pip install -r /www/sites/foo.bar/requirements.txt
 
     def test_clean(self, *args):
 
-        clean(self.c, 'foo.bar')
+        fabfile.clean(self.c, 'foo.bar')
 
         self.c.sudo.assert_has_calls([
             call('supervisorctl stop foo.bar'),
@@ -310,14 +292,12 @@ pip install -r /www/sites/foo.bar/requirements.txt
             call('rm -f /etc/nginx/sites-enabled/foo.bar'),
         ])
 
-
-### env
+# env
 
     def test_env_cmd(self, *args):
-        remote_env_cmd(self.c, "FOO=BAR", "printenv FOO")
+        fabfile.remote_env_cmd(self.c, "FOO=BAR", "printenv FOO")
         self.c.run.assert_called_once_with('FOO=BAR printenv FOO')
 
     def test_env_sudo(self, *args):
-        remote_env_sudo(self.c, "FOO=BAR", "printenv FOO")
+        fabfile.remote_env_sudo(self.c, "FOO=BAR", "printenv FOO")
         self.c.sudo.assert_called_once_with('FOO=BAR printenv FOO')
-        
--- test_sites.py
+++ test_sites.py
@@ -1,66 +1,67 @@
 from unittest.mock import patch, mock_open, MagicMock
+
+import fabfile
 import pytest
-from fabfile import *
+import textwrap
+
 
 @pytest.fixture
 def c():
     return MagicMock()
 
+
 @pytest.fixture(params=[8000, 8001])
-def p(request):
+def port(request):
     return request.param
 
+
 @patch('fabfile.REMOTE_ROOT', '/www')
-@patch('invoke.tasks.isinstance') # necessary for mocking
-def test_site_nginx(c, p):
+@patch('invoke.tasks.isinstance')  # necessary for mocking
+def test_site_nginx(c, port):
     m = mock_open()
     with patch('fabfile.open', m, create=True):
         with patch('fabfile.os.makedirs') as mk:
-            generate_site_nginx(c, 'foo.bar', p)
-    #c.local.assert_called_once_with(
-    #    'mkdir -p sites/foo.bar/etc/nginx/sites-available'
-    #)
+            fabfile.generate_site_nginx(c, 'foo.bar', port)
     mk.assert_called_once_with('sites/foo.bar/etc/nginx/sites-available')
     m.assert_called_once_with(
         'sites/foo.bar/etc/nginx/sites-available/foo.bar',
         'w'
     )
-    m().write.assert_called_with(
-"""\
-server {
-    server_name foo.bar;
-    location / {
-        proxy_pass http://localhost:%d;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-    }
-    location /static {
-        alias  /www/sites/foo.bar/static/;
-    }
-}
-""" % p
-    )
+    m().write.assert_called_with(textwrap.dedent(
+        f"""\
+        server {{
+            server_name foo.bar;
+            location / {{
+                proxy_pass http://localhost:{port};
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+            }}
+            location /static {{
+                alias  /www/sites/foo.bar/static/;
+            }}
+        }}
+        """
+    ))
 
 
 @patch('fabfile.REMOTE_ROOT', '/www')
 @patch('fabfile.user', 'www')
-@patch('invoke.tasks.isinstance') # necessary for mocking
-def test_site_supervisor(c, p):
+@patch('invoke.tasks.isinstance')  # necessary for mocking
+def test_site_supervisor(c, port):
     m = mock_open()
     with patch('fabfile.open', m, create=True):
         with patch('fabfile.os.makedirs') as mk:
-            generate_site_supervisor(c, 'foo.bar', 'baz', 'bla', p)
+            fabfile.generate_site_supervisor(c, 'foo.bar', 'baz', 'bla', port)
     mk.assert_called_once_with('sites/foo.bar/etc/supervisor/conf.d')
     m.assert_called_once_with(
         'sites/foo.bar/etc/supervisor/conf.d/foo.bar.conf',
         'w'
     )
-    m().write.assert_called_with(
-f"""\
-[program:foo.bar]
-command = /www/sites/foo.bar/venv3/bin/gunicorn baz:bla -b localhost:{p}
-directory = /www/sites/foo.bar/src
-user = www
-"""
-    )
-
+    m().write.assert_called_with(textwrap.dedent(
+        f"""\
+        [program:foo.bar]
+        command = /www/sites/foo.bar/venv3/bin/gunicorn baz:bla -b localhost:{port}
+        directory = /www/sites/foo.bar/src
+        user = www
+        """
+    ))
